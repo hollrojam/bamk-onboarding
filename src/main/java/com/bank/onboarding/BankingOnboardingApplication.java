71e87811a6843d62e635d4d06b2b1b014efa345f
HollmanRojas <hollmanerojas@gmail.com>
2026-02-10 13:32:14 -0500

    Resolve merge conflicts

---
+++ src/main/java/com/bank/onboarding/BankingOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.bank.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingOnboardingApplication.class, args);
+	}
+
+}

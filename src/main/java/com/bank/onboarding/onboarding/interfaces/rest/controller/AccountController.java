62d624395f7f27a9f22466a3d8d6c51f8a8a1eb2
HollmanRojas <hollmanerojas@gmail.com>
2026-02-11 23:57:14 -0500

    add configurations

--- src/main/java/com/bank/onboarding/onboarding/interfaces/rest/controller/AccountController.java
+++ src/main/java/com/bank/onboarding/onboarding/interfaces/rest/controller/AccountController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/accounts")
 @RequiredArgsConstructor
 public class AccountController {
--- src/main/java/com/bank/onboarding/onboarding/interfaces/rest/controller/CustomerController.java
+++ src/main/java/com/bank/onboarding/onboarding/interfaces/rest/controller/CustomerController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
